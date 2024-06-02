33c778b03c523cf4330bd96a66178ad1c5c3e105
Gustavo Blas Duran <gustavoblasduran@gmail.com>
2024-06-01 21:04:39 -0600

    Organizing Airflow instance

---
+++ dags/my_packages/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+# requiered contents of the package that you create
+
+# it can alse be empty
+
+# airflow expects that this file exists for all packages you added
---
+++ dags/test_module.py
@@ -0,0 +1,18 @@
+from airflow.decorators import dag, task
+
+import pendulum
+
+from my_packages.packages_a.module_a import TestClass
+
+
+@dag(schedule=None, start_date=pendulum.datetime(2024, 5, 1), catchup=False)
+def test_module():
+
+    @task
+    def test_task():
+        print(TestClass.my_time())
+
+    test_task()
+
+dag = test_module()
+
