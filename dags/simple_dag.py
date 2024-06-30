b9b10628929082e76cf4e0a0d8da6add712f37c4
Gustavo Blas Duran <gustavoblasduran@gmail.com>
2024-06-30 11:44:58 -0600

    FileSensor example

--- dags/simple_dag.py
+++ dags/simple_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
+from airflow.sensors.filesystem import FileSensor
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 
@@ -9,8 +10,9 @@ default_args = {
     'retry_delay':timedelta(minutes=5)
 }
 
-def _downloading_data(my_param, ds):
-    print(my_param)
+def _downloading_data(**kwargs):
+    with open('/tmp/my_file.txt', 'w') as f:
+        f.write('my_data')
 
 with DAG(dag_id='simple_dag', 
         start_date=days_ago(3),
@@ -23,18 +25,16 @@ with DAG(dag_id='simple_dag',
 
     downloading_data = PythonOperator(
         task_id = 'downloading_data',
-        python_callable=_downloading_data,
-        op_kwargs={'my_param': 42}
+        python_callable=_downloading_data
     )
 
     task_1 = DummyOperator(
         task_id = 'task_1'
     )
 
-    task_2 = DummyOperator(
-        task_id = 'task_2'
-    )
-
-    task_3 = DummyOperator(
-        task_id = 'task_3'
+    waiting_for_data = FileSensor(
+        task_id='waiting_for_data',
+        fs_conn_id='fs_default', # ID of the connection
+        filepath='my_file.txt',
+        poke_interval=30 # want to check every 30 seconds
     )
